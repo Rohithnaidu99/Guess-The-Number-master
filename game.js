64431751612883d191e30ab1384cc7356ede48e5
Rohithnaidu99 <131711205+Rohithnaidu99@users.noreply.github.com>
2024-02-18 18:28:16 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,32 @@
+const $submitbutton = document.getElementById("submit");
+const $message = document.getElementById("message");
+const $lives = document.getElementById("lives");
+
+var guessnumber = Math.floor(Math.random() * 100);
+localStorage.setItem("ans",guessnumber)
+var lives = 5;
+var message;
+
+
+var input = document.getElementById("number-input");
+input.addEventListener("keyup", function(event) {
+    if (event.code === "Enter") {
+        event.preventDefault();
+        document.getElementById("submit").click();
+    }
+});
+
+
+$submitbutton.onclick = () => {
+  var userip = document.getElementById("number-input").value;
+  lives--;
+  if (userip == guessnumber) location.href = "./win.html";
+  else if (lives == 0) location.href = "./lose.html";
+  else if (userip > guessnumber) message = "Your Guess is  high";
+  else if (userip < guessnumber) message = "Your Guess is  low";
+
+  $message.style.display = "inhertit";
+  //debugger
+  $message.innerHTML = message;
+  $lives.innerHTML = lives;
+};
